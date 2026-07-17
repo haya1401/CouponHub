0280d98154622df811bee18ad03a5b8cb9e5678b
haya1401 <haua112233@icloud.com>
2026-07-17 10:53:40 +0300

    Add FeaturedCoupons component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import StoreLogos from "./components/StoreLogos";
+import FeaturedCoupons from "./components/FeaturedCoupons";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Hero />
       <StoreLogos />
+      <FeaturedCoupons />
     </>
   );
 }
