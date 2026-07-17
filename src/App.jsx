1b9990f087a5f19742ae5da200af73048dce23e1
haya1401 <haua112233@icloud.com>
2026-07-17 10:47:44 +0300

    Add StoreLogos component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import StoreLogos from "./components/StoreLogos";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <StoreLogos />
     </>
   );
 }
