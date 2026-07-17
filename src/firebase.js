e2da077fd12d2ae81eb388545939866e56323e5f
haya1401 <haua112233@icloud.com>
2026-07-17 13:56:16 +0300

    Initialize Firebase app and Firestore/Auth services

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAmQAe4uKdPD_LDYMhrhEKy3WMhiAjQiP8",
+  authDomain: "couponhub-70fc5.firebaseapp.com",
+  projectId: "couponhub-70fc5",
+  storageBucket: "couponhub-70fc5.firebasestorage.app",
+  messagingSenderId: "544276776162",
+  appId: "1:544276776162:web:6617c611e06f932afccabb"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export default app;
