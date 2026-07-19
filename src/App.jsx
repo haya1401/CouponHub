6c8e9b218c3e368c182c4c15c0ae5d0d0b473768
haya1401 <haua112233@icloud.com>
2026-07-19 09:19:44 +0300

    Refactor routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import StoreLogos from "./components/StoreLogos";
 import FeaturedCoupons from "./components/FeaturedCoupons";
 import Categories from "./components/Categories";
 import Footer from "./components/Footer";
+
+import Stores from "./pages/Stores";
 import Coupons from "./pages/Coupons";
+import StoreCoupons from "./pages/StoreCoupons";
 import CategoriesPage from "./pages/Categories";
 import Contact from "./pages/Contact";
-import Stores from "./pages/Stores";
 import AdminLogin from "./pages/AdminLogin";
 import Dashboard from "./pages/Dashboard";
 
@@ -31,14 +33,39 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/stores" element={<Stores />} />
+
         <Route path="/coupons" element={<Coupons />} />
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* صفحة كوبونات متجر معين */}
+        <Route
+          path="/coupons/:store"
+          element={<StoreCoupons />}
+        />
+
+        <Route
+          path="/categories"
+          element={<CategoriesPage />}
+        />
+
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+
+        <Route
+          path="/admin"
+          element={<AdminLogin />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
       </Routes>
-       <Footer />
+
+      <Footer />
     </>
   );
 }
