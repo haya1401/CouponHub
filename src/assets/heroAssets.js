4c4682ceb039c0d24a98a49e4afba2b136b8ac91
haya1401 <haua112233@icloud.com>
2026-07-23 13:32:26 +0300

    Add hero asset imports and exports

--- src/assets/heroAssets.js
+++ src/assets/heroAssets.js
@@ -1,1 +1,12 @@
+import coin from "./hero/coin.webp";
+import coupon from "./hero/coupon.webp";
+import gift from "./hero/gift.webp";
+import shoppingBag from "./hero/shopping-bag.webp";
 
+
+export {
+coin,
+coupon,
+gift,
+shoppingBag
+};
