bdef73836653c562ce844186cd18017122077047
haya1401 <haua112233@icloud.com>
2026-07-23 17:58:46 +0300

    Add FloatingObjects component with images

--- src/components/FloatingObjects.jsx
+++ src/components/FloatingObjects.jsx
@@ -1,1 +1,39 @@
+import "./FloatingObjects.css";
 
+import coin from "../assets/hero/coin.webp";
+import coupon from "../assets/hero/coupon.webp";
+import bag from "../assets/hero/shopping-bag.webp";
+import gift from "../assets/hero/gift.webp";
+
+export default function FloatingObjects() {
+
+  return (
+
+    <div className="floating-wrapper">
+
+      {/* العملات */}
+
+      <img src={coin} className="float coin1" alt="" />
+      <img src={coin} className="float coin2" alt="" />
+      <img src={coin} className="float coin3" alt="" />
+      <img src={coin} className="float coin4" alt="" />
+      <img src={coin} className="float coin5" alt="" />
+
+      {/* الكوبونات */}
+
+      <img src={coupon} className="float coupon1" alt="" />
+      <img src={coupon} className="float coupon2" alt="" />
+
+      {/* الحقيبة */}
+
+      <img src={bag} className="float bag1" alt="" />
+
+      {/* الهدية */}
+
+      <img src={gift} className="float gift1" alt="" />
+
+    </div>
+
+  );
+
+}
