74a619710bfcf0386b97e7649626ce6944508cce
haya1401 <haua112233@icloud.com>
2026-07-23 15:11:09 +0300

    Refactor AnimatedBackground component structure

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -3,30 +3,46 @@ import "./AnimatedBackground.css";
 
 export default function AnimatedBackground(){
 
+
 return (
 
-<div className="animated-background">
+<div 
+className="animated-background"
+aria-hidden="true"
+>
 
 
 <div className="glow glow-blue"></div>
 
+
 <div className="glow glow-gold"></div>
+
+
 
 
 <div className="particles">
 
-<span></span>
-<span></span>
-<span></span>
-<span></span>
-<span></span>
+
 <span></span>
 
+<span></span>
+
+<span></span>
+
+<span></span>
+
+<span></span>
+
+<span></span>
+
+
 </div>
+
 
 
 </div>
 
 );
+
 
 }
