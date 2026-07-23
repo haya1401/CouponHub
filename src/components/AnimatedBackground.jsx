e38434529ece4fb885b26c89c8e1d7fe66da2ae2
haya1401 <haua112233@icloud.com>
2026-07-23 14:23:19 +0300

    Add AnimatedBackground component with styles

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -1,1 +1,32 @@
+import "./AnimatedBackground.css";
 
+
+export default function AnimatedBackground(){
+
+return (
+
+<div className="animated-background">
+
+
+<div className="glow glow-blue"></div>
+
+<div className="glow glow-gold"></div>
+
+
+<div className="particles">
+
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+
+</div>
+
+
+</div>
+
+);
+
+}
