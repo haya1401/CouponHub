5bee5785be0ef16b6efa276d901cf820012bccf4
haya1401 <haua112233@icloud.com>
2026-07-25 09:50:51 +0300

    Replace Hero component with HeroSlider

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import Hero from "./components/Hero";
+// استبدال المكون القديم بالسلايدر المتحرك الجديد
+import HeroSlider from "./components/HeroSlider";
 import StoreLogos from "./components/StoreLogos";
 import FeaturedCoupons from "./components/FeaturedCoupons";
 import Categories from "./components/Categories";
@@ -15,127 +16,60 @@ import StoreCoupons from "./pages/StoreCoupons";
 import AdminLogin from "./pages/AdminLogin";
 import Dashboard from "./pages/Dashboard";
 
-
 export default function App() {
-
-
   return (
-
-
     <div className="app">
-
-
       <Header />
 
-
-
       <Routes>
-
-
-
         {/* الصفحة الرئيسية */}
-
         <Route
-
           path="/"
-
           element={
-
             <>
-
-              <Hero />
+              {/* السلايدر المتحرك في الواجهة */}
+              <HeroSlider />
 
               <FeaturedCoupons />
 
               <StoreLogos />
 
               <Categories />
-
             </>
-
           }
-
         />
-
-
-
-
 
         {/* صفحة جميع المتاجر */}
-
         <Route
-
           path="/stores"
-
           element={<Stores />}
-
         />
-
-
-
-
 
         {/* جميع الكوبونات */}
-
         <Route
-
           path="/coupons"
-
           element={<Coupons />}
-
         />
-
-
-
-
 
         {/* كوبونات متجر واحد */}
-
         <Route
-
           path="/store/:id"
-
           element={<StoreCoupons />}
-
         />
-
-
-
-
 
         {/* الإدارة */}
-
         <Route
-
           path="/admin"
-
           element={<AdminLogin />}
-
         />
-
-
 
         <Route
-
           path="/dashboard"
-
           element={<Dashboard />}
-
         />
-
-
-
       </Routes>
 
-
-
       <Footer />
-
-
-
     </div>
-
-
   );
-
 }
